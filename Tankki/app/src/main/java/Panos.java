0c39f5f3e7233b499be0664be3b56d2bf2460f6d
visimon <teemu.jaask@gmail.com>
2015-03-06 14:37:35 +0200

    luokkia

---
+++ Tankki/app/src/main/java/Panos.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Otso on 6.3.2015.
+ */
+public class Panos {
+    // ammus luokka u know
+
+    public final int DAMAGE = 100;
+
+    public Panos () {
+        
+    }
+}
---
+++ Tankki/app/src/main/java/com/tankkipeli/teemun/otson/tankki/Tankki.java
@@ -0,0 +1,20 @@
+package com.tankkipeli.teemun.otson.tankki;
+
+/**
+ * Created by Otso on 6.3.2015.
+ */
+public class Tankki {
+    /*
+    tankki luokka
+    */
+    public int speed;
+
+    //tankki rakentaja
+    public Tankki (int tankSpeed) {
+        speed = tankSpeed;
+    }
+
+    public void moveTank () {
+
+    }
+}
