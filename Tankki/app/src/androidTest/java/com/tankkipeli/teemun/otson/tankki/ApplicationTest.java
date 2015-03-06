c619fca38ba434b65ef4393c1e27cf3392cc216a
Odemon <otso.leppala@gmail.com>
2015-03-06 12:29:32 +0200

    android projekti

---
+++ Tankki/app/src/androidTest/java/com/tankkipeli/teemun/otson/tankki/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.tankkipeli.teemun.otson.tankki;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
